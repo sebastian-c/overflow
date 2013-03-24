1394f9410a0027184e7f9640294ad91a0c9dff07
Sebastian <sebastian.campbell@sydney.edu.au>
2013-03-24 19:20:07 +1100

    Added Ananda Mahto's readSO function.

---
+++ R/readSO.R
@@ -0,0 +1,19 @@
+#' Read in StackOverflow table-formatted data
+#' 
+#' A wrapper for \link{read.table}. Reads a table from text or from the clipboard and creates a data.frame from it.
+#' 
+#' @author Ananda Mahto
+#'
+#' @param sep character. Determines the field separator character passed to \code{read.table}.
+#' @param header logical. Determines whether the first row consists of names of variables.
+#' @return A data.frame as read.table produces. 
+#' @note By default, stringsAsFactors is FALSE which is different to the R default.
+#'
+#' @export
+
+readSO <- function(sep = "", header = TRUE, stringsAsFactors = FALSE) {
+  suppressWarnings(
+    read.table(text = gsub("^#", "", readLines("clipboard")),
+               header = header, stringsAsFactors = stringsAsFactors,
+               sep = sep))
+}
