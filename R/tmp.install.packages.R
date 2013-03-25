8fc6d42cd3c14ac65bcd44da5780bb65c8de31bf
Julien Barnier <julien@nozav.org>
2013-03-25 09:26:44 +0100

    Check if tempdir() is already in .libPaths()

--- R/tmp.install.packages.R
+++ R/tmp.install.packages.R
@@ -10,12 +10,18 @@
 ##' won't be accessible as it won't be in the library trees anymore, and as the
 ##' files are installed in a temporary directory they should be deleted upon next
 ##' reboot (this depends on your operating system).
+##'
+##' @note
+##' You can find alternative functions in the original question and answers on
+##' StackOverflow :
+##'
+##' \url{http://stackoverflow.com/questions/14896941/install-an-r-package-temporarily-only-for-the-current-session}
 ##' 
 ##' @param package a package name, or a character vector of package names
 ##' @param dependencies argument passed to \code{install.packages}. Default to TRUE.
 ##' @param ... other arguments passed to \code{install.packages}
 ##' @export
-##' @author Julien Barnier
+##' @author Julien Barnier <julien@@nozav.org>
 ##' @seealso \code{\link{install.packages}}, \code{\link{.libPaths}}
 ##' @examples
 ##'
@@ -29,7 +35,9 @@ tmp.install.packages <- function(package, dependencies=TRUE, ...) {
   ## Add 'path' to .libPaths, and be sure that it is not
   ## at the first position, otherwise any package during
   ## this session would be installed into 'path'
-  firstpath <- .libPaths()[1]
-  .libPaths(c(firstpath, path))
+  if (!(path %in% .libPaths())) {
+    firstpath <- .libPaths()[1]
+    .libPaths(c(firstpath, path))
+  }
   install.packages(package, dependencies=dependencies, lib=path, ...)
 }
