25f27748f0bf5c7596d53ede5f69d9120b6e2cb1
Sebastian <sebastian.campbell@sydney.edu.au>
2014-03-25 12:41:01 +0100

    Added package description.

---
+++ R/package.R
@@ -0,0 +1,12 @@
+#' Helper functions for answering R StackOverflow questions
+#' 
+#' This package is focused on providing helper functions to answer StackOverflow
+#' questions. It includes functions to transfer code and create random datasets.
+#' 
+#' @docType package
+#' @name overflow-package
+#' @aliases overflow
+#' 
+#' @author Sebastian Campbell, Ananda Mahto, Julien Barnier, Tyler Rinker
+
+NULL
